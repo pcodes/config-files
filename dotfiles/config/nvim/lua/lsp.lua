2c7d2b57faddb71302c26af8884d66adcd63e137
Pat Cody <patcody028@gmail.com>
2023-03-19 23:45:14 -0700

    Remove old lua LSP config

--- dotfiles/config/nvim/lua/lsp.lua
+++ dotfiles/config/nvim/lua/lsp.lua
@@ -32,29 +32,29 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
-require'lspconfig'.sumneko_lua.setup {
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = runtime_path
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true)
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {enable = false}
-        }
-    },
-    capabilities = capabilities
-}
+-- require'lspconfig'.sumneko_lua.setup {
+--     settings = {
+--         Lua = {
+--             runtime = {
+--                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+--                 version = 'LuaJIT',
+--                 -- Setup your lua path
+--                 path = runtime_path
+--             },
+--             diagnostics = {
+--                 -- Get the language server to recognize the `vim` global
+--                 globals = {'vim'}
+--             },
+--             workspace = {
+--                 -- Make the server aware of Neovim runtime files
+--                 library = vim.api.nvim_get_runtime_file("", true)
+--             },
+--             -- Do not send telemetry data containing a randomized but unique identifier
+--             telemetry = {enable = false}
+--         }
+--     },
+--     capabilities = capabilities
+-- }
 
 require'lspconfig'.bashls.setup{
     on_attach = on_attach
